0ea4fa9500e7619e530798f0856a0124ba8a95d0
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2023-11-01 09:30:49 -0400

    comment system CRUd

--- db/migration.sql
+++ db/migration.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS story_parts (
                                            child_cannon_time Timestamp DEFAULT NOW() + INTERVAL '24 hours', -- Null unless the story part is "cannon"
                                            first UUID REFERENCES story_parts(id), -- optimization for going to the top, not sure if to keep or not
                                            parent UUID REFERENCES story_parts(id), -- used to search for "child" stories
-                                           points Int NOT NULL DEFAULT 0, -- cache for upvote - downvote
+                                           score Int NOT NULL DEFAULT 0, -- cache for upvote - downvote
                                            child UUID REFERENCES story_parts(id) -- update when a child becomes cannon
 );
 
@@ -39,6 +39,18 @@ CREATE INDEX IF NOT EXISTS idx_cannon_expire ON story_parts(child_cannon_time DE
 CREATE INDEX IF NOT EXISTS idx_user_token ON users USING hash(token); -- Helps search for an user with a hash1
 CREATE INDEX idx_parent_part_id ON story_parts USING hash(parent); -- This needs to be tested, to see if hashing has collisions
 
-CREATE INDEX idx_points ON story_parts(points);
+CREATE INDEX idx_story_points ON story_parts(points);
+
+CREATE TABLE IF NOT EXISTS comments(
+                                    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+                                    writer Text NOT NULL,
+                                    body Text NOT NULL,
+                                    created Timestamp NOT NULL DEFAULT NOW(),
+                                    modified Timestamp DEFAULT NULL,
+                                    score Int NOT NULL DEFAULT 0,
+                                    snippet UUID NOT NULL REFERENCES story_parts(id)
+);
+
+CREATE INDEX idx_comment_points ON comments(points);
 
 -- TODO: Vote system, comments, caching, "place" system,
