3238b100027548fe07c9ec7775579c460655250b
DianaCiorgovean <diana.ciorgovean02@e-uvt.ro>
2023-12-02 19:51:07 +0200

    First commit for Login still WIP

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,38 @@
+
+<svelte:head>
+	<title>Getting started</title>
+	<meta name="description" content="It's time to create your own story!" />
+</svelte:head>
+
+
+	<h1>Storytime</h1>
+	<script> 
+		import {useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
+
+const form = useForm();
+</script>
+
+<form use:form>
+<h1>Login</h1>
+
+<input type="email" name="email" use:validators={[required, email]} />
+<HintGroup for="email">
+  <Hint on="required">This is a mandatory field</Hint>
+  <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+</HintGroup>
+
+<input type="password" name="password" use:validators={[required]} />
+<Hint for="password" on="required">This is a mandatory field</Hint>
+
+<button disabled={!$form.valid}>Login</button>
+</form>
+<pre>
+{JSON.stringify($form, null, " ")}
+</pre>
+
+<style>
+  :global(.touched:invalid) {
+	  border-color: red;
+	  outline-color: red;
+  }
+</style>
