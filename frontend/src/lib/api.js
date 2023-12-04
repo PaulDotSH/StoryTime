03dcd021dcb53ee26ceda748eea9da39b19c0a16
Bobitza <vlad.gozman@yahoo.com>
2023-12-04 22:13:20 +0200

    Refactoring frontend part

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { error } from '@sveltejs/kit';
+
+const base = 'http://127.0.0.1:5431'; // Ensure you include the protocol (http/https)
+
+async function send({ method, path, data, token }) {
+    const headers = {
+		'Content-Type': 'application/json'
+	};
+
+    if (token) {
+        headers['Authorization'] = `Token ${token}`;
+    }
+
+    try {
+        const response = await axios[method.toLowerCase()](`${base}/${path}`, data, { headers });
+
+        if (response.status === 200) {
+            return response.statusText || {};
+        }
+
+        throw error();
+    } catch (error) {
+        throw error;
+    }
+}
+
+export function get(path, token) {
+    return send({ method: 'GET', path, token });
+}
+
+export function del(path, token) {
+    return send({ method: 'DELETE', path, token });
+}
+
+export function post(path, data, token) {
+    return send({ method: 'POST', path, data, token });
+}
+
+export function put(path, data, token) {
+    return send({ method: 'PUT', path, data, token });
+}
