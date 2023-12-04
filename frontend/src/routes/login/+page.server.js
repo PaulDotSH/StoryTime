034b6326d4e861ecf89128b4808dd1cdc1838268
DianaCiorgovean <diana.ciorgovean02@e-uvt.ro>
2023-12-04 22:45:28 +0200

    Updated login

---
+++ frontend/src/routes/login/+page.server.js
@@ -0,0 +1,28 @@
+import { fail, redirect } from '@sveltejs/kit';
+import * as api from '$lib/api.js';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals }) {
+	if (locals.user) throw redirect(307, '/');
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		//console.log(data);
+		const body = await api.post('login', {
+				username: data.get('username'),
+				password: data.get('password')
+		});
+
+		if (body.errors) {
+			return fail(401, body);
+		}
+
+		const value = btoa(JSON.stringify(body.user));
+		cookies.set('jwt', value, { path: '/' });
+
+		throw redirect(307, '/');
+	}
+};
